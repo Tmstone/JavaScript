84199410f19c714eca8d9e0cd895cacca9909598
Terence Stone <terence.stone25@gmail.com>
2020-02-05 09:32:39 -0500

    Added methods to the Ninja Class

--- ninja.js
+++ ninja.js
@@ -3,7 +3,7 @@ function ninja (name, health) {
     var speed = 3;
     var strength = 3;
     var stats = function() {
-        console.log('This is a private method for ', self.name);
+        console.log('This is a private method for', self.name);
         console.log(self);
     }
     this.name = name;
@@ -11,14 +11,15 @@ function ninja (name, health) {
     //writing methods
     //sayName() - This should log that Ninja's name to the console.
     this.sayName = function(){
-        console.log('Ninja name: ', this.name);
+        console.log('My ninja name is:', this.name);
     }
     //showStats() - This should show the Ninja's name, strength, speed, and health.
     this.showStats = function() {
+        console.log('Name: ' + this.name + ', Health: ' + this.health + ', Speed: ' + speed + ', Strength: ' + strength);
         stats();
     }
     //drinkSake() - This should add +10 Health to the Ninja
-    this.drinkSake() = function() {
+    this.drinkSake = function() {
         this.health += 10;
     }
 
@@ -26,7 +27,7 @@ function ninja (name, health) {
 }
 var Mike = new ninja('Mike', 175);
 Mike.sayName();
-Mike.drinkSake();
+//Mike.drinkSake();
 Mike.showStats();
 /* 
 name
