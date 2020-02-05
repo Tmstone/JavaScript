db73f3cc4451f964c0faeca303f27e0957b80aa6
Terence Stone <terence.stone25@gmail.com>
2020-02-05 10:49:50 -0500

    recreated Ninja Class with prototype

--- ninja.js
+++ ninja.js
@@ -31,3 +31,36 @@ Mike.sayName();
 Mike.drinkSake();
 Mike.showStats();
 */
+
+function Ninja (name) {
+
+    this.name = name;
+    this.health = 100;
+    //private variables
+    var speed = 3;
+    var strength = 3;
+    //showStats() - This should show the Ninja's name, strength, speed, and health.
+    this.showStats = function() {
+        console.log('Name: ' + this.name + ', Health: ' + this.health + ', Speed:' + speed + ', Strength: ' + strength);
+        return this;
+        //stats();
+    }
+    //writing methods
+    //sayName() - This should log that Ninja's name to the console.
+    Ninja.prototype.sayName = function(){
+        console.log('My ninja name is:', this.name);
+        return this;
+    }   
+    
+    //drinkSake() - This should add +10 Health to the Ninja
+    Ninja.prototype.drinkSake = function() {
+        this.health += 10;
+        return this;
+    }
+
+
+}
+var Mike = new Ninja('Mike', 175);
+Mike.sayName();
+Mike.drinkSake();
+Mike.showStats();
