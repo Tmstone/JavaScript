bd997e5e891c60d3a4b5ed08853f7da1d0f953f2
Terence Stone <terence.stone25@gmail.com>
2020-02-11 17:15:20 -0500

    creating a reduce function

--- js_library.js
+++ js_library.js
@@ -35,8 +35,56 @@ function filter(array, callback) {
     }
     return results;
 }
-
 results = filter(myArray, function(element) {
     return !isNaN(element);
 });
+results = filter(myArray, function(element) {
+    return element % 2 === 0; 
+});
+console.log(results);
+//reject function
+function reject(array, callback) {
+    const results = [];
+    for(let index = 0; index < array.length; index ++) {
+        if (!callback(array[index], index) ) {
+            results.push(array[index]);
+        }
+    }
+    return results;
+}
+results = reject(myArray, function(element) {
+    return isNaN(element);
+}); 
 
+//find function
+function find(array, callback) {
+    for(let index = 0; index < array.length; index++) {
+        if(callback(array[index], index)) {
+            return array[index];
+        }
+    }
+}
+console.log(find(results, function(element) {
+    return element === 4;
+}));
+//reduce function
+function reduce(array, callback, memo) {
+    const results = [].concat(array);
+
+    if(memo === undefinded) {
+        memo = results.pop();
+    }
+    for (let i = 0; i < array.length; i++) {
+       memo = callback(memo, array[i], i);
+    }
+    return memo;
+}
+
+results = reduce(results, add);
+
+function add(num1, num2, num3) {
+    return num1+ num2 + num3;
+}
+console.log(memo);
+
+//refractor loops  and call the function
