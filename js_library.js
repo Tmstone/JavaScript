15aa08e14ed974b59641c1fad4da28457d132915
Terence Stone <terence.stone25@gmail.com>
2020-02-11 12:47:46 -0500

    creating a replica of a js library

---
+++ js_library.js
@@ -0,0 +1,7 @@
+function each(arr, callback) {
+    for(let index = 0; index ,arry.length; index++) {
+        callback(array[index], index);
+    }
+}
+
+const myArray = ['1','2,','3','4','5','6','7','8','9',10];
