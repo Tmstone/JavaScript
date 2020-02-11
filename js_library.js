7a06ed831bf7227f329de751f76a26c6886fc133
Terence Stone <terence.stone25@gmail.com>
2020-02-11 13:56:36 -0500

    creating a callback and testing it

--- js_library.js
+++ js_library.js
@@ -1,7 +1,12 @@
-function each(arr, callback) {
-    for(let index = 0; index ,arry.length; index++) {
+function each(array, callback) {
+    for(let index = 0; index ,array.length; index++) {
         callback(array[index], index);
+        console.log(array[index])
     }
 }
 
-const myArray = ['1','2,','3','4','5','6','7','8','9',10];
+const myArray = ['1','2','3','4','5','6','7','8','9','10'];
+
+each(myArray, function(element, index){
+    console.log(`element: ${element} and index: ${index}`);
+});
